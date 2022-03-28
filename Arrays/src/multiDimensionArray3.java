9fbf15aa9ac16492fe8df2c92c83bdf9d1534d42
Ophney Mbedzi <97296828+ophney@users.noreply.github.com>
2022-03-28 17:08:30 +0200

    Add files via upload

---
+++ Arrays/src/multiDimensionArray3.java
@@ -0,0 +1,39 @@
+public class multiDimensionArray3 {
+    public static  void main(String [] args){
+        int [][] myArray=new int[100][30];
+        int[][] o=getRandomArray(myArray);
+        System.out.print("Original multi-dimensional array of the sizes 100 outer 30 inner:\n ");
+        for(int[] x:o){
+            for (int y:x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.print("incremented values in the multi-dimensional array:\n ");
+        for(int i = 0; i < myArray.length; i++){
+            for (int j=0; j<myArray[i].length; j++) {
+                o[i][j] = o[i][j] + 1;
+            }
+        }
+        for(int[] x:o){
+            for(int y:x) {
+                System.out.print(y + " ");
+            }
+            System.out.println( );
+        }
+        System.out.println();
+    }
+
+    private static int[][] getRandomArray(int[][] arr){
+        for(int i=0;i<arr.length;i++) {
+            for(int j=0;j<arr[i].length;j++) {
+
+                arr[i][j] = (int) (Math.random() * 50);
+            }
+        }
+        return arr;
+    }
+}
+
---
+++ Arrays/src/multiDimensionArray4.java
@@ -0,0 +1,42 @@
+public class multiDimensionArray4 {
+    public static  void main(String [] args){
+        int [][] myArray=new int[4][3];
+        int[][] o=getRandomArray(myArray);
+        System.out.print("Original multi-dimensional array of the sizes 100 outer 30 inner:\n ");
+        for(int[] x:o){
+            for (int y:x) {
+                if(y % 2!=0 && y % 8 == 0 ) {
+                    System.out.print(y + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.print("incremented values in the multi-dimensional array:\n ");
+        for(int i = 0; i < myArray.length; i++){
+            for (int j=0; j<myArray[i].length; j++) {
+                o[i][j] = o[i][j] + 1;
+            }
+        }
+        for(int[] x:o){
+            for(int y:x) {
+                if(y % 2 != 0 && y % 8 == 0 ) {
+                    System.out.print(y + " ");
+                }
+            }
+            System.out.println( );
+        }
+        System.out.println();
+    }
+
+    private static int[][] getRandomArray(int[][] arr){
+        for(int i=0;i<arr.length;i++) {
+            for(int j=0;j<arr[i].length;j++) {
+
+                arr[i][j] = (int) (Math.random() * 150000);
+            }
+        }
+        return arr;
+    }
+}
